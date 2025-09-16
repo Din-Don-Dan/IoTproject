5638d937cdd2d30de283a414eb997546b06e2d5f
Din-Don-Dan <jaco.dan03@gmail.com>
2025-09-16 23:11:19 +0200

    revert port change

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const parser = new ReadlineParser({
     delimiter: "\r\n"
 });
 
-const port = new SerialPort({ path:'COM5', 
+const port = new SerialPort({ path:'/dev/ttyACM0', 
     baudRate: 9600
 });
 
